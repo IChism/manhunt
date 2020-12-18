92495543750db203cd4a774b70c29b7d95bde46e
IChism <isaiahchis@hotmail.com>
2020-12-17 20:02:14 -0600

    added basic plugin essentials

---
+++ src/com/mypackage/Manhunt.java
@@ -0,0 +1,15 @@
+package com.mypackage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Manhunt extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
