745915495dc2131df318fbc575eddfdfb8cc4d18
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2020-12-17 21:49:03 -0600

    Added Color

--- src/com/mypackage/Manhunt.java
+++ src/com/mypackage/Manhunt.java
@@ -8,7 +8,11 @@ public class Manhunt extends JavaPlugin {
     @Override
     public void onEnable() {
         Objects.requireNonNull(this.getCommand("manhuntinfo")).setExecutor(new CommandManhuntinfo());
+        Objects.requireNonNull(this.getCommand("getcompass")).setExecutor(new GetCompass());
+        getServer().getPluginManager().registerEvents(new ManhuntGameListener(), this);
     }
+
+
 
     @Override
     public void onDisable() {
